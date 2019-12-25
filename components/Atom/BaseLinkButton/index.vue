830eda6a8ca75fe554d6392723c55a85f4027581
Tomonari Takai <takai01010100@gmail.com>
2019-12-25 19:02:30 +0900

    Update Style at BaseLinkButton

--- components/Atom/BaseLinkButton/index.vue
+++ components/Atom/BaseLinkButton/index.vue
@@ -22,8 +22,7 @@ export default {
     },
     url: {
       type: String,
-      require: true,
-      default: 'https://ja.nuxtjs.org/',
+      default: '###',
     },
   },
   computed: {
@@ -41,25 +40,33 @@ export default {
 
 <style lang="sass" scoped>
 @mixin _structure()
-  height: 48px
-  width: 160px
-  padding: 0 16px
+  min-height: 3rem
+  min-width: 10rem
+  max-width: 90vw
+  padding: 0.5rem 1rem
   display: inline-flex
   align-items: center
   justify-content: center
-  line-height: 1
+  line-height: 1.2
 
-@mixin _button($_color)
-  @include _structure()
+@mixin _skin($_color)
   border-radius: 4px
   border: 1px solid $_color
   color: $_color
   text-decoration: none
-  text-align: center
   &:hover
     color: white
     background-color: $_color
+  // Extra
+  white-space: pre-line
+  word-wrap: break-word
+  overflow-wrap: break-word
 
+@mixin _button($_color)
+  @include _structure()
+  @include _skin($_color)
+
+// Style Instance ===============
 .buttonGreen
   @include _button(#3b8070)
 
