d9b04edaa37fd2244210dfe352dee373db005f21
Tomonari Takai <takai01010100@gmail.com>
2019-12-30 18:46:38 +0900

    WIP: watch window innersize

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,41 @@
 <template lang="pug">
-  div
+  div(:class="classNameObject")
     nuxt
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+
 export default {
   name: 'LayoutDefault',
+  computed: {
+    ...mapState(['isDesktop', 'isTablet', 'isMobile']),
+    classNameObject() {
+      return {
+        'is-desktop': this.isDesktop,
+        'is-tablet': this.isTablet,
+        'is-mobile': this.isMobile,
+      }
+    },
+  },
+  // created() {
+  //   this.setDeviceFlag(window.innerWidth)
+  // },
+  beforeMount() {
+    this.hadleResize()
+  },
+  mounted() {
+    window.addEventListener('resize', this.hadleResize())
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.hadleResize())
+  },
+  methods: {
+    ...mapActions(['setDeviceFlag']),
+    hadleResize() {
+      this.setDeviceFlag(window.innerWidth)
+      console.log('Fire hadleResize')
+    },
+  },
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -2,16 +2,40 @@
 const zeroPadding = (num, length = 2) => {
   const stringNumber = ('0000' + num).slice(-length)
   return stringNumber
-  // console.log('zeroPadding', stringNumber)
-  // return parseInt(stringNumber, 10)
 }
 
 // Vuex Modules ==============================
 export const state = () => ({
   dateObj: {},
+  innerWidth: 0,
+  // ref: https://hashimotosan.hatenablog.jp/entry/2019/05/28/164834
+  breakPoint: {
+    large: 960,
+    medium: 768,
+    small: 560,
+  },
+  isDesktop: true,
+  isTablet: false,
+  isMobile: false,
 })
 
 export const getters = {
+  isDesktopLarge: (state) => {
+    return state.innerWidth > state.breakPoint.large
+  },
+  isDesktop: (state) => {
+    const max = state.innerWidth <= state.breakPoint.large
+    const min = state.innerWidth > state.breakPoint.medium
+    return max && min
+  },
+  isTablet: (state) => {
+    const max = state.innerWidth <= state.breakPoint.medium
+    const min = state.innerWidth > state.breakPoint.small
+    return max && min
+  },
+  isMobile: (state) => {
+    return state.innerWidth <= state.breakPoint.small
+  },
   YYYY: (state) => {
     const year = state.dateObj.getFullYear()
     return zeroPadding(year, 4)
@@ -19,8 +43,6 @@ export const getters = {
   MM: (state) => {
     // (January gives 0)
     const M = state.dateObj.getMonth() + 1
-    // const MM = zeroPadding(M)
-    // console.log('getters: MM', M, MM)
     return zeroPadding(M)
   },
   DD: (state) => {
@@ -45,12 +67,53 @@ export const mutations = {
   setDateObject: (state, dateObj) => {
     state.dateObj = dateObj
   },
+  setInnerWidth: (state, width) => {
+    state.innerWidth = width
+  },
+  setFlagDesktop: (state) => {
+    state.isDesktop = true
+    state.isTablet = false
+    state.isMobile = false
+  },
+  setFlagTablet: (state) => {
+    state.isDesktop = false
+    state.isTablet = true
+    state.isMobile = false
+  },
+  setFlagMobile: (state) => {
+    state.isDesktop = false
+    state.isTablet = false
+    state.isMobile = true
+  },
 }
 
 export const actions = {
   getDate({ commit }) {
     const date = new Date()
-    // console.log('actions getDate', date)
     commit('setDateObject', date)
+  },
+  updateInnerWidth({ commit }, innerWidth) {
+    commit('setInnerWidth', innerWidth)
+  },
+  setDeviceFlag({ commit, dispatch, state }, innerWidth) {
+    dispatch('updateInnerWidth', innerWidth)
+    console.log('setDeviceFlag', innerWidth)
+
+    // Like a Getters
+    const max = state.innerWidth <= state.breakPoint.medium
+    const min = state.innerWidth > state.breakPoint.small
+    const isTablet = max && min
+    const isMobile = state.innerWidth <= state.breakPoint.small
+
+    if (isMobile) {
+      commit('setFlagMobile')
+      console.log('setDeviceFlag: isMobile')
+    } else if (isTablet) {
+      commit('setFlagTablet')
+      console.log('setDeviceFlag: isTablet')
+    } else {
+      commit('setFlagDesktop')
+      console.log('setDeviceFlag: isDesktop')
+    }
   },
 }
